2f76b05b8b8013908accdbfecd5d60deaa869fb3
SRIKANTH VARIPALLI <varipallisrikanth@gmail.com>
2018-03-05 19:22:26 +0800

    added dependencies for lang3

--- src/main/java/com/sree/training/Application.java
+++ src/main/java/com/sree/training/Application.java
@@ -1,10 +1,15 @@
 package com.sree.training;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
+    }
+    public int countWords(String words){
+    	String[] separateWords = StringUtils.split(words , ' ');
+    	return (separateWords == null) ? 0 : separateWords.length;
     }
     public void greet(){
     	List<String> greetings = new ArrayList<>();
@@ -19,5 +24,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	int count = app.countWords("this is a string");
+	System.out.println("word count: " + count);
     }
 }
